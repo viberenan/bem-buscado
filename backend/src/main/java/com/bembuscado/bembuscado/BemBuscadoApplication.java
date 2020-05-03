c18ef71e7d4eb4f6a69009bba95a383bdcae6832
System Administrator <root@MacBook-Pro-de-Renan.local>
2020-05-03 15:05:41 -0300

    Commit Inicial

---
+++ backend/src/main/java/com/bembuscado/bembuscado/BemBuscadoApplication.java
@@ -0,0 +1,13 @@
+package com.bembuscado.bembuscado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemBuscadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemBuscadoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bembuscado/bembuscado/BemBuscadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bembuscado.bembuscado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemBuscadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
