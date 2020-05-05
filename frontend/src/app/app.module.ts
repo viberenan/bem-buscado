368029db42b5e6ff871059cd8e06afec6a4878ec
Renan Couto <rennanccouto@gmail.com>
2020-05-05 12:17:42 -0300

    Desenvolvimento do Header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,38 +1,20 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NouisliderModule } from 'ng2-nouislider';
-import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
-import { AgmCoreModule } from '@agm/core';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
-import { NavbarComponent } from './components/navigation/navbar/navbar.component';
-import { PainelBuscaComponent } from './components/navigation/painel-busca/painel-busca.component';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-    declarations: [ 
-        AppComponent,
-        NouisliderModule,
-        JwBootstrapSwitchNg2Module,
-        AgmCoreModule,
-        NavbarComponent,
-        PainelBuscaComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-        ComponentsModule,
-        ExamplesModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
