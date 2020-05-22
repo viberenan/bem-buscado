c19b7aaae7c35309ef274a8cbdfacb15f69e9581
Renan Couto <rennanccouto@gmail.com>
2020-05-21 21:57:18 -0300

    Configuraçao JPA

--- backend/src/main/java/com/bembuscado/api/BemBuscadoApplication.java
+++ backend/src/main/java/com/bembuscado/api/BemBuscadoApplication.java
@@ -1,4 +1,4 @@
-package com.bembuscado.bembuscado;
+package com.bembuscado.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/bembuscado/api/BemBuscadoApplicationTests.java
+++ backend/src/test/java/com/bembuscado/api/BemBuscadoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bembuscado.bembuscado;
+package com.bembuscado.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
